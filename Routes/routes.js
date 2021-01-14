01b3bf9eb784a55d76078d9518d78157768bbdc1
Lea Dalibard <leadalibard@gmail.com>
2021-01-14 17:03:18 +0100

    Saving last updates

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,5 +1,5 @@
-const Users= require("./../Models/users");
-const passport=require('passport');
+const Users = require("./../Models/users");
+const passport = require('passport');
 const express = require('express');
 const path = require('path');
 const router = express.Router();
@@ -13,7 +13,19 @@ router.get('/login', function (req, res) {
     res.render('login', {})
 })
 
+router.post('/login', function (req, res) {
+    res.render('login', {})
+})
+
 router.get('/register', function (req, res) {
     res.render('register', {})
 })
+
+router.post('/register', function (req, res) {
+    Users.register(new Users({username: req.body.username, email: req.body.email}), req.body.password), function (err) {
+        console.log('error')
+    };
+    res.redirect('/login')
+})
+
 module.exports = router;
