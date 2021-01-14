2e438f8582136b5ecda48902a58e1f4c31df8760
Lea Dalibard <leadalibard@gmail.com>
2021-01-14 15:18:26 +0100

    Saving passport localStrategy, serialize and deserialize

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieSession = require('cookie-session');
-var app = express();
 var mongoose=require ('mongoose');
 const port = 3000;
-
 const Users= require("./Models/users");
-
 const passport=require('passport');
-
+var app = express();
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(cookieSession({
     name: 'session',
     keys: ['key1', 'key2']
@@ -21,14 +16,15 @@ app.use(cookieSession({
 
 app.post('/login', passport.authenticate('local', { successRedirect: '/',
     failureRedirect: '/login' }));
-
-mongoose.connect('mongodb://localhost:3000/Users', {useNewUrlParser: true});
-Users.findOne(function(error, result) { /* ... */ });
-
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+passport.use(new LocalStrategy(Users.authenticate()));
+passport.serializeUser(Users.serializeUser());
+passport.deserializeUser(Users.deserializeUser());
+
+mongoose.connect('mongodb://localhost:3000/Users', {useNewUrlParser: true});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
